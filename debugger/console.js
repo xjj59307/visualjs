8a2fa96e531d243d5091fa0cf1051cbfcba264c7
xjj59307 <xjj59307@gmail.com>
2012-11-26 22:38:11 +0900

    簡単なコンソールを作った

--- debugger/console.js
+++ debugger/console.js
@@ -1,35 +1,82 @@
-$(document).ready(function(){
-    var console = $('<div class="console">');
-    $('body').append(console);
-    var controller1 = console.console({
-        promptLabel: 'debug> ',
-        commandValidate:function(line){
-            if (line == "") return false;
-            else return true;
-        },
-        commandHandle:function(line){
-            try {
-                var ret = eval(line);
-                if (typeof ret != 'undefined') return ret.toString();
-                else return true;
+var sys = {
+
+    callFrame: null,
+
+    pause: function() {
+        chrome.debugger.sendCommand(this.debuggee, 'Debugger.enable');
+        chrome.debugger.sendCommand(this.debuggee, 'Debugger.pause');
+    },
+
+    parse: function(cmd) {
+        if (cmd === 'pause') {
+            this.pause();
+            return 'paused';
+        }
+        else {
+            var async_response = null;
+            chrome.debugger.sendCommand(
+                this.debuggee,
+                'Debugger.evaluateOnCallFrame',
+                { 
+                    callFrameId: sys.callFrame.callFrameId,
+                    expression: cmd,
+                },
+                function(response) {
+                    async_response = response;
+                }
+            ); 
+
+            var value = async_response.result.value;
+
+            if (typeof value != 'undefined') {
+                return value.toString();
             }
-            catch (e) { return e.toString(); }
-        },
-        autofocus:true,
-        animateScroll:true,
-        promptHistory:true,
-    });
+            else return true;
+        }
+    },
+
+    createConsole: function() {
+        self = this;
+
+        $(document).ready(function() {
+            var console = $('<div class="console">');
+            $('body').append(console);
+            var controller1 = console.console({
+                promptLabel: 'debug> ',
+                commandValidate: function(line) {
+                    if(line == "") return false;
+                    else return true;
+                },
+                commandHandle: function(cmd) {
+                    try { 
+                        return self.parse(cmd);
+                    }
+                    catch (e) { return e.toString(); }
+                },
+                autofocus: true,
+                animateScroll: true,
+                promptHistory: true,
+            });
+        });
+    },
+
+    initialize: function() {
+        var tabId = parseInt(window.location.search.substring(1));
+        this.debuggee = { tabId: tabId };
+
+        this.createConsole();
+    }
+
+}
+
+chrome.debugger.onEvent.addListener(function(debuggee, method, params) {
+    switch (method) {
+        case 'Debugger.paused':
+            sys.callFrame = params.callFrames[0];
+        break;
+        default:
+            break;
+    }
 });
 
-var eval = function(expr) {
-    var tabId = debuggeeId.tabId;
-
-    chrome.debugger.sendCommand(
-        tabId,
-        "Runtime.evaluate",
-        { expression: expr },
-        function(response) {
-            alert(response.result);                    
-        }
-    );
-}
+sys.initialize();
