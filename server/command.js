1ad8315510a2c1e708bbedd799e310f82719a513
xjj59307 <xjj59307@gmail.com>
2013-02-02 00:36:29 +0900

    setbreakpoint

--- server/command.js
+++ server/command.js
@@ -10,4 +10,23 @@ Command.prototype.requireScripts = function (callback) {
 	this.debug.send(request, callback);
 };
 
+Command.prototype.setBreakpoint = function (scriptName, line, condition, callback) {
+	var request = {
+		command: 'script',
+		target: scriptName,
+		line: line,
+		condition: condition
+	};
+
+	this.debug.send(request, callback);	
+};
+
+Command.prototype.continue = function (callback) {
+	var request = {
+		command: 'continue'
+	};
+
+	this.debug.send(request, callback);
+};
+
 module.exports = Command;
--- server/debugger.js
+++ server/debugger.js
@@ -2,14 +2,16 @@ var pDebug = require('pDebug').pDebug;
 var Command = require('./command');
 
 var eventHandler = function (event) {
-    // console.log("Event: " + event);
+    console.log("Event: " + event);
 };
 
 var Debug = function () {
     this.debug = new pDebug({
         eventHandler: eventHandler
     });
+    this.command = new Command(this.debug);
     this.scripts = {};
+    this.currentScript = null;
 };
 
 var natives = process.binding('natives');
@@ -18,6 +20,7 @@ Debug.prototype._addScript = function (script) {
     this.scripts[script.id] = script;
     if (script.name) {
         script.isNative = (script.name.replace('.js', '') in natives) || script.name === 'node.js';
+        this.currentScript = !script.isNative && script;
     }
 };
 
@@ -27,13 +30,19 @@ Debug.prototype.connect = function () {
     this.debug.connect(function() { 
         console.log('connect!'); 
 
-        var command = new Command(self.debug);
-        command.requireScripts(function (request, response) {
+        self.command.requireScripts(function (request, response) {
             for (var i = 0; i < response.body.length; ++i) {
                 self._addScript(response.body[i]);
             }
         });
     });
 };
+
+Debug.prototype.setBreakPoint = function (line) {
+    var self = this;
+    
+    // this.command.setBreakpoint(this.currentScript.name, line);
+    // this.command.continue();
+}
 
 module.exports = Debug;
