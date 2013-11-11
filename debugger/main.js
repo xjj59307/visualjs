cbcc23f577990704cf108cc8e700cd0d81c2885d
junjianx <xjj59307@gmail.com>
2013-11-11 15:22:42 +0900

    Automize terminal client starting.

--- debugger/main.js
+++ debugger/main.js
@@ -1,5 +1,15 @@
-var Interface = require('./cmd_interface');
+var Interface = require('./cmd-interface'),
+    exec = require('child_process').exec;
 
+// Open debugger in node.
+var debuggee = process.argv[2] || "debugger/sort.js";
+var child = exec('node --debug-brk=8000 ' + debuggee, function (error, stdout, stderr) {
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
+
+// Start client for debugger in terminal.
 var stdin = process.stdin;
 var stdout = process.stdout;
 var interface = new Interface(stdin, stdout);
