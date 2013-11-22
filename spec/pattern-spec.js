55bb6c63abc74a28e0018a357dd6ea374b43f9be
xjj59307 <xjj59307@gmail.com>
2013-11-22 18:32:15 +0900

    Unit test for pattern.js

--- spec/pattern-spec.js
+++ spec/pattern-spec.js
@@ -1,11 +1,24 @@
+var _ = require('underscore');
 var Pattern = require('../debugger/pattern');
 
 describe('Pattern', function() {
     var tree = require('../debugger/ast.json');
+    var pattern = new Pattern(tree.pattern);
 
-    it('pattern.parseExecClause', function() {
-        var pattern = new Pattern(tree.pattern);
-        expect(true).toBe(true);
+    it('action name', function() {
+        expect(pattern.name).toBe('operator');
+    });
+
+    it('first match', function() {
+        var match = _.first(pattern.matches); 
+        expect(match.actionName).toBe('plus');
+        expect(match.conditionCode).toBe('this.op === 0');
+
+        var environment = {
+            op: '\'+\'',
+            parent: 'node'
+        };
+        expect(match.environment).toEqual(environment);
     });
 });
 
