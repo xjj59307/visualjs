2eaa4cd69a90eb6527606ebfb9e4070694de9a79
xjj59307 <xjj59307@gmail.com>
2013-12-25 16:43:42 +0900

    Update sample code.

--- debugger/sort.js
+++ debugger/sort.js
@@ -1,4 +1,12 @@
-var left = { op: 2, left: 1, right: 2};
-var right = { op: 3, left: 3, right: 4};
-var root = { op: 0, left: left, right: right};
-console.log(root);
+// Generate a math expression tree with height 4.
+var generate = function(depth) {
+  // Every leaf is valued 1.
+  if (depth > 2) return 1;
+
+  var operator = Math.floor(Math.random()*10) % 4;
+  return { op: operator, left: generate(depth+1), right: generate(depth+1) };
+};
+
+var root = generate(0);
+var left = root.left;
+var right = root.right;
