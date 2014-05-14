8e30ca088e5ac54026d340907853e4005bcfbb2d
xjj59307 <xjj59307@gmail.com>
2014-05-14 22:24:49 +0900

    terminal support mousewheel

--- public/javascripts/lib/require-config.js
+++ public/javascripts/lib/require-config.js
@@ -1,24 +1,25 @@
 var require = {
-    baseUrl: "../javascripts",
-    paths: {
-        "layout": "object-graph/layout",
-        "utility": "object-graph/utility",
-        "graph": "object-graph/graph",
-        "acyclic": "object-graph/acyclic",
-        "rank": "object-graph/rank",
-        "order": "object-graph/order",
-        "normalize": "object-graph/normalize",
-        "position": "object-graph/position",
-        "prim": "object-graph/prim"
-    },
-    shim: {
-        "lib/d3.v3": { exports: "d3" },
-        "lib/underscore": { exports: "_" },
-        "lib/buckets": { exports: "buckets" },
-        "lib/priority-queue": { exports: "priorityQueue" },
-        "lib/jquery-1.8.2": { exports: "$" },
-        "lib/ace/ace": { exports: "ace" },
-        "lib/jquery.terminal-0.8.7": {}
-    }
+  baseUrl: "../javascripts",
+  paths: {
+    "layout": "object-graph/layout",
+    "utility": "object-graph/utility",
+    "graph": "object-graph/graph",
+    "acyclic": "object-graph/acyclic",
+    "rank": "object-graph/rank",
+    "order": "object-graph/order",
+    "normalize": "object-graph/normalize",
+    "position": "object-graph/position",
+    "prim": "object-graph/prim"
+  },
+  shim: {
+    "lib/d3.v3": { exports: "d3" },
+    "lib/underscore": { exports: "_" },
+    "lib/buckets": { exports: "buckets" },
+    "lib/priority-queue": { exports: "priorityQueue" },
+    "lib/jquery-1.8.2": { exports: "$" },
+    "lib/ace/ace": { exports: "ace" },
+    "lib/jquery.terminal-0.8.7": {},
+    "lib/jquery.mousewheel": {},
+  }
 };
 
